6c2ab7d2deaa370b56a0326b3320d3691476fd0e
JonathasGit <jonathas-25@hotmail.com>
2021-12-12 20:30:14 -0200

    #1 Código da aula 1

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let canvas = document.getElementById ("cobrinha");
 let context = canvas.getContext("2d"); /**/ 
-let box = 32;
-
-function criarBG(){
-    context.fillStyle = "black";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+let box = 32; /*32 pixels cada quadrado*/
+let cobrinha = [];
+cobrinha[0] = {
+    x: 8 * box,
+    y: 8 * box
 }
 
-criarBR();
+function criarBG(){
+   context.fillStyle = "lightgreen"; /*Estilo do Canvas*/ 
+    context.fillRect(0, 0, 16 * box, 16 * box);/*Desenhar onde vai acontecer o jogo*/ 
+}
+
+
+function criarCobrinha(){
+    for(i=0; < cobrinha.length; i++){
+        context.fillStyle = "red";
+        context.fillRect(cobrinha[i].x, cobrinha[i].y, box, box);
+    }
+}
+
+
+criarBR(); 
+criarCobrinha();
