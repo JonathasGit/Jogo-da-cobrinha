239ae6a7de18bac7c3d771cd2fe1669bd59a3d41
JonathasGit <jonathas-25@hotmail.com>
2021-12-12 21:19:11 -0200

    #2 Códigos aula 2

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ cobrinha[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG(){
    context.fillStyle = "lightgreen"; /*Estilo do Canvas*/ 
@@ -14,12 +15,33 @@ function criarBG(){
 
 
 function criarCobrinha(){
-    for(i=0; < cobrinha.length; i++){
+    for(i=0; i < cobrinha.length; i++){
         context.fillStyle = "red";
         context.fillRect(cobrinha[i].x, cobrinha[i].y, box, box);
     }
 }
 
+function iniciarJogo(){
+    criarBR(); 
+    criarCobrinha();
 
-criarBR(); 
-criarCobrinha();
+    let cobrinhaX = cobrinha[0].x;
+    let cobrinhaY = cobrinha[0].y;
+
+    if(direction == "right") cobrinhaX += box; 
+    if(direction == "left") cobrinhaX -= box;
+    if(direction == "up") cobrinhaY -= box;
+    if(direction == "down") cobrinhaY += box;
+
+    cobrinha.pop();
+
+    let newHead = {
+        x: cobrinhaX,
+        y: cobrinhaY        
+    }
+    cobrinha.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100); /*Intervalo do jogo */
+
+
